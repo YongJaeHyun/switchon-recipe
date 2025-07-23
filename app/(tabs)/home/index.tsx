6b289d52859fdd3c7c5ee64e3a9750b9cd18b4ed
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-07-23 18:31:33 +0900

    ✨ 홈 화면 Refresh Control 기능 추가 및 컴포넌트 분리

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,16 +1,30 @@
 import Feather from '@expo/vector-icons/Feather';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { logout } from 'api/supabaseAPI';
+import RecentRecipes from 'components/home/RecentRecipes';
+import RecipeCreation from 'components/home/RecipeCreation';
+import SavedRecipes from 'components/home/SavedRecipes';
 import { Image } from 'expo-image';
-import { Link, router } from 'expo-router';
-import { Button, Pressable, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { Button, Pressable, RefreshControl, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useUserStore } from 'stores/userStore';
 import colors from 'tailwindcss/colors';
-import { recipeButtonBG } from 'utils/assets';
+import { useRecipeStore } from '../../../stores/recipeStore';
 
 export default function HomeScreen() {
   const userInfo = useUserStore((state) => state.user);
+
+  const [refreshing, setRefreshing] = useState(false);
+  const fetchRecentRecipes = useRecipeStore((state) => state.fetchRecentRecipes);
+  const fetchSavedRecipes = useRecipeStore((state) => state.fetchSavedRecipes);
+
+  const handleRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchRecentRecipes();
+    await fetchSavedRecipes();
+    setRefreshing(false);
+  }, [fetchRecentRecipes, fetchSavedRecipes]);
 
   const logoutAndRedirect = async () => {
     await logout();
@@ -20,44 +34,35 @@ export default function HomeScreen() {
   if (!userInfo) logoutAndRedirect();
   return (
     <SafeAreaView className="flex-1 bg-neutral-100 px-5">
-      <View className="mt-8 flex-row items-center justify-between">
-        <Text className="text-neutral-600">오늘도 화이팅!</Text>
-        <View className="flex-row items-center gap-5">
-          <Feather name="settings" size={24} />
-          <Pressable className="h-10 w-10 overflow-hidden rounded-full">
-            <Image style={{ width: '100%', height: '100%' }} source={userInfo?.avatar_url} />
-          </Pressable>
-        </View>
-      </View>
-      <Text style={{ fontFamily: 'roboto' }} className="text-4xl font-bold">
-        1일차
-      </Text>
-
-      <View
-        className="my-6"
-        style={{ borderBottomWidth: 2, borderBottomColor: colors.neutral[400] }}
-      />
-
-      <Link
-        href={'/(tabs)/home/recipeCreation'}
-        className="h-44 w-full overflow-hidden rounded-xl bg-white shadow-lg">
-        <View className="flex-1 flex-row">
-          <View className="flex-[3] justify-between px-4 py-3">
-            <View className="flex-row items-center gap-2">
-              <Text className="text-2xl font-bold">레시피 제작</Text>
-              <MaterialIcons name="arrow-forward-ios" size={24} color={colors.neutral[400]} />
-            </View>
-            <View>
-              <Text className="text-lg">내가 가지고 있는 재료로</Text>
-              <Text className="text-lg">레시피 제작!</Text>
-            </View>
-          </View>
-          <View className="h-full w-full flex-[2]">
-            <Image style={{ width: '100%', height: '100%' }} source={recipeButtonBG} />
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}>
+        <View className="flex-row items-center justify-between pt-4">
+          <Text className="text-neutral-600">오늘도 화이팅!</Text>
+          <View className="flex-row items-center gap-5">
+            <Feather name="settings" size={24} />
+            <Pressable className="h-10 w-10 overflow-hidden rounded-full">
+              <Image style={{ width: '100%', height: '100%' }} source={userInfo?.avatar_url} />
+            </Pressable>
           </View>
         </View>
-      </Link>
-      <Button title="로그아웃" onPress={logoutAndRedirect} />
+        <Text style={{ fontFamily: 'roboto' }} className="text-4xl font-bold">
+          1일차
+        </Text>
+
+        <View
+          className="my-6"
+          style={{ borderBottomWidth: 2, borderBottomColor: colors.neutral[400] }}
+        />
+
+        <View className="gap-12">
+          <RecipeCreation />
+          <SavedRecipes refreshing={refreshing} />
+          <RecentRecipes refreshing={refreshing} />
+          <Button title="로그아웃" onPress={logoutAndRedirect} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ components/home/RecipeCreation.tsx
@@ -0,0 +1,31 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
+import colors from 'tailwindcss/colors';
+import { recipeButtonBG } from 'utils/assets';
+
+export default function RecipeCreation() {
+  return (
+    <Link
+      href={'/(tabs)/home/recipeCreation'}
+      className="h-48 w-full overflow-hidden rounded-xl bg-white shadow-lg">
+      <View className="flex-row">
+        <View className="flex-[3] justify-between px-4 py-3">
+          <View className="flex-row items-center gap-2">
+            <Text className="text-2xl font-bold">레시피 제작</Text>
+            <MaterialIcons name="arrow-forward-ios" size={24} color={colors.neutral[400]} />
+          </View>
+          <View>
+            <Text className="text-lg">내가 가지고 있는 재료로</Text>
+            <Text className="text-lg">레시피 제작!</Text>
+          </View>
+        </View>
+        <View className="h-full w-full flex-[2]">
+          <Image style={{ width: '100%', height: '100%' }} source={recipeButtonBG} />
+        </View>
+      </View>
+    </Link>
+  );
+}
