f37acc4b6331115de8e9141be1c6cd725039b390
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-07-25 23:24:25 +0900

    ✨ 현재 주차 진행도 표시 기능 추가

--- components/home/HomeHeader.tsx
+++ components/home/HomeHeader.tsx
@@ -6,6 +6,8 @@ import { Link } from 'expo-router';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { useUserStore } from 'stores/userStore';
+import { getWeekAndDay } from 'utils/date';
+import { getWeekColor } from 'utils/getWeekColor';
 
 interface HomeHeaderProps {
   bottomSheetRef: React.RefObject<BottomSheetMethods>;
@@ -13,6 +15,7 @@ interface HomeHeaderProps {
 
 export default function HomeHeader({ bottomSheetRef }: HomeHeaderProps) {
   const userInfo = useUserStore((state) => state.user);
+  const { week, day } = getWeekAndDay(userInfo.start_date);
 
   return (
     <View>
@@ -25,39 +28,36 @@ export default function HomeHeader({ bottomSheetRef }: HomeHeaderProps) {
         </View>
       </View>
       <View className="flex-row items-end gap-3">
-        <Text style={{ fontFamily: 'roboto' }} className="text-4xl font-bold">
-          1주차
+        <Text
+          style={{ fontFamily: 'roboto' }}
+          className={`text-4xl font-bold ${getWeekColor(week)}`}>
+          {week}주차 {week >= 5 && '(유지기)'}
         </Text>
         <Pressable onPress={() => bottomSheetRef.current?.expand()}>
           <Feather name="settings" size={18} />
         </Pressable>
       </View>
 
-      <View className="mt-6 flex-row gap-5">
-        <View className="items-center gap-2">
-          <Text className="">1일</Text>
-          <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
-            <MaterialIcons name="check" size={20} />
-          </View>
-        </View>
-        <View className="items-center gap-2">
-          <Text className="">1일</Text>
-          <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
-            <MaterialIcons name="check" size={20} />
-          </View>
-        </View>
-        <View className="items-center gap-2">
-          <Text className="">1일</Text>
-          <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
-            <MaterialIcons name="check" size={20} />
-          </View>
-        </View>
-        <View className="items-center gap-2">
-          <Text className="">1일</Text>
-          <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
-            <MaterialIcons name="check" size={20} />
-          </View>
-        </View>
+      <View className="mt-6 flex-row gap-5 px-2">
+        {Array(8)
+          .fill(null)
+          .map((_, i) =>
+            i === 7 ? (
+              <View key={`${week + 1}주차`} className="ml-2 items-center gap-2">
+                <Text className={`font-semibold ${getWeekColor(week + 1)}`}>{week + 1}주차</Text>
+                <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
+                  <MaterialIcons name="arrow-forward" size={20} />
+                </View>
+              </View>
+            ) : (
+              <View key={`${week}주차 ${i + 1}일`} className="items-center gap-2">
+                <Text className="text-lg">{i + 1}일</Text>
+                <View className="h-8 w-8 items-center justify-center rounded-full bg-neutral-300">
+                  {i + 1 <= day && <MaterialIcons name="check" size={20} />}
+                </View>
+              </View>
+            )
+          )}
       </View>
     </View>
   );
