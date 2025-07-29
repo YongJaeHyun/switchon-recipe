39311a00929287465c0d94e91698bd5b6be8ca6d
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-07-29 16:48:17 +0900

    💄 홈 탭의 레시피 제작 버튼 Ripple 애니메이션 적용

--- components/home/RecipeCreation.tsx
+++ components/home/RecipeCreation.tsx
@@ -5,25 +5,31 @@ import { Link } from 'expo-router';
 import React from 'react';
 import { Text, View } from 'react-native';
 import colors from 'tailwindcss/colors';
+import RippleButton from '../common/RippleButton';
 
 export default function RecipeCreation() {
   return (
-    <Link href={'/recipeCreation'} className="h-52 w-full">
-      <View className="flex-row overflow-hidden rounded-xl bg-white shadow-lg">
-        <View className="flex-[3] justify-between px-6 py-5">
-          <View className="flex-row items-center gap-2">
-            <Text className="text-3xl font-bold">레시피 제작</Text>
-            <MaterialIcons name="arrow-forward-ios" size={24} color={colors.neutral[400]} />
+    <Link href={'/recipeCreation'} className="h-52 w-full shadow-xl" asChild>
+      <RippleButton
+        className="overflow-hidden bg-transparent"
+        rippleColor={colors.neutral[400]}
+        rounded="xl">
+        <View className="flex-row bg-white">
+          <View className="flex-[3] justify-between px-6 py-5">
+            <View className="flex-row items-center gap-2">
+              <Text className="text-3xl font-bold">레시피 제작</Text>
+              <MaterialIcons name="arrow-forward-ios" size={24} color={colors.neutral[400]} />
+            </View>
+            <View>
+              <Text className="text-lg">내가 가지고 있는 재료로</Text>
+              <Text className="text-lg">레시피 제작!</Text>
+            </View>
           </View>
-          <View>
-            <Text className="text-lg">내가 가지고 있는 재료로</Text>
-            <Text className="text-lg">레시피 제작!</Text>
+          <View className="h-full w-full flex-[2]">
+            <Image style={{ width: '100%', height: '100%' }} source={recipeButtonBG} />
           </View>
         </View>
-        <View className="h-full w-full flex-[2]">
-          <Image style={{ width: '100%', height: '100%' }} source={recipeButtonBG} />
-        </View>
-      </View>
+      </RippleButton>
     </Link>
   );
 }
