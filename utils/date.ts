744450e3bc9dbd03d2536eb45a0b7933a5b32344
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-07-30 19:36:02 +0900

    ✨ 현재로부터 몇 주 전의 날짜를 쉽게 구할 수 있는 유틸함수 추가

--- utils/date.ts
+++ utils/date.ts
@@ -19,4 +19,14 @@ const getKoreanToday = () => {
   return koreaTime.toISOString().split('T')[0];
 };
 
-export { getKoreanToday, getWeekAndDay };
+const getKoreanDateWeeksAgo = (weeksAgo: number) => {
+  const now = new Date();
+  const offset = 9 * 60 * 60 * 1000; // UTC+9
+  const koreaTime = new Date(now.getTime() + offset);
+  const msPerWeek = 7 * 24 * 60 * 60 * 1000;
+
+  const weeksAgoDate = new Date(koreaTime.getTime() - weeksAgo * msPerWeek);
+  return weeksAgoDate.toISOString().split('T')[0];
+};
+
+export { getKoreanDateWeeksAgo, getKoreanToday, getWeekAndDay };
