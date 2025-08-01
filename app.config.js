daaf76fe202772a66e91fe17a2f1a1ddcf7f5bb5
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-08-02 04:17:52 +0900

    🐛 컬러 코드 인식 버그 수정

--- app.config.js
+++ app.config.js
@@ -44,12 +44,12 @@ module.exports = {
         {
           image: './assets/splash.png',
           resizeMode: 'cover',
-          backgroundColor: '#fff',
+          backgroundColor: '#ffffff',
           imageWidth: 190,
           android: {
             adaptiveIcon: {
               foregroundImage: './assets/adaptive-icon.png',
-              backgroundColor: '#fff',
+              backgroundColor: '#ffffff',
             },
           },
         },
@@ -65,6 +65,7 @@ module.exports = {
       resizeMode: 'contain',
       backgroundColor: '#ffffff',
     },
+    assets: ['./assets/fonts/'],
     assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
