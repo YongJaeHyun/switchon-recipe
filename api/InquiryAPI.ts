0b4a1f4b62a72a1f701e57e0a71fa342e32053ec
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-08-08 09:23:06 +0900

    🥅 에러 처리 추가

--- api/InquiryAPI.ts
+++ api/InquiryAPI.ts
@@ -7,49 +7,58 @@ import { showErrorToast, showSuccessToast } from 'utils/showToast';
 const selectAll = async (): Promise<InquiryDB[]> => {
   const userId = useUserStore.getState().id;
 
-  const { data, error } = await supabase
-    .from('inquiry')
-    .select('*')
-    .eq('uid', userId)
-    .order('created_at', { ascending: false });
+  try {
+    const { data, error } = await supabase
+      .from('inquiry')
+      .select('*')
+      .eq('uid', userId)
+      .order('created_at', { ascending: false });
 
-  if (error) {
+    if (error) {
+      Sentry.captureException(error);
+      showErrorToast({
+        text1: 'DB 에러 발생',
+        text2: '에러 정보가 관리자에게 전달되었습니다. 빠른 시일 내에 조치하겠습니다.',
+        error,
+      });
+      return [];
+    }
+
+    return data;
+  } catch (error) {
     Sentry.captureException(error);
-    showErrorToast({
-      text1: 'DB 에러 발생',
-      text2: '에러 정보가 관리자에게 전달되었습니다. 빠른 시일 내에 조치하겠습니다.',
-      error,
-    });
-    return [];
   }
-
-  return data;
 };
 
-const insertInquiryToDB = async (inquiry: Partial<InquiryDB>) => {
-  const { data, error } = await supabase
-    .from('inquiry')
-    .insert({ ...inquiry })
-    .select();
+const insert = async (inquiry: Partial<InquiryDB>) => {
+  try {
+    const { data, error } = await supabase
+      .from('inquiry')
+      .insert({ ...inquiry })
+      .select()
+      .single();
 
-  if (error) {
-    showErrorToast({
-      text1: 'DB 에러 발생',
-      text2: '에러 정보가 관리자에게 전달되었습니다. 빠른 시일 내에 조치하겠습니다.',
-      error,
-    });
-    throw error;
-  } else {
-    showSuccessToast({
-      text1: '문의사항이 등록되었습니다.',
-      text2: '문의 남겨주셔서 감사합니다 :)',
-    });
+    if (error) {
+      showErrorToast({
+        text1: 'DB 에러 발생',
+        text2: '에러 정보가 관리자에게 전달되었습니다. 빠른 시일 내에 조치하겠습니다.',
+        error,
+      });
+      throw error;
+    } else {
+      showSuccessToast({
+        text1: '문의사항이 등록되었습니다.',
+        text2: '문의 남겨주셔서 감사합니다 :)',
+      });
+    }
+
+    return data;
+  } catch (error) {
+    Sentry.captureException(error);
   }
-
-  return data;
 };
 
 export const InquiryAPI = {
   selectAll,
-  insertInquiryToDB,
+  insert,
 };
