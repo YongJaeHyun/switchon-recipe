ccf85940dd9c97cb607a3f4802f69e4af2602574
YongjaeHyun <dltjrrbs2020@gmail.com>
2025-08-12 12:18:06 +0900

    💄 홈 날짜 진행도 UI 개선

--- utils/getWeekColor.ts
+++ utils/getWeekColor.ts
@@ -1,8 +1,28 @@
-export const getWeekColor = (week: number) => {
-  if (week === 1) return 'text-rose-500/70';
-  else if (week === 2) return 'text-yellow-500';
-  else if (week === 3) return 'text-lime-600';
-  else if (week === 4) return 'text-violet-500/65';
-  else if (week >= 5) return 'text-black';
+const getWeekColor = (week: number) => {
+  if (week === 1) return 'text-week-1';
+  else if (week === 2) return 'text-week-2';
+  else if (week === 3) return 'text-week-3';
+  else if (week === 4) return 'text-week-4';
+  else if (week >= 5) return 'text-week-5';
   else return '';
 };
+
+const getWeekBGColor = (week: number) => {
+  if (week === 1) return 'bg-week-1';
+  else if (week === 2) return 'bg-week-2';
+  else if (week === 3) return 'bg-week-3';
+  else if (week === 4) return 'bg-week-4';
+  else if (week >= 5) return 'bg-week-5';
+  else return '';
+};
+
+const getWeekBorderColor = (week: number) => {
+  if (week === 1) return 'border-week-1';
+  else if (week === 2) return 'border-week-2';
+  else if (week === 3) return 'border-week-3';
+  else if (week === 4) return 'border-week-4';
+  else if (week >= 5) return 'border-week-5';
+  else return '';
+};
+
+export { getWeekBGColor, getWeekBorderColor, getWeekColor };
