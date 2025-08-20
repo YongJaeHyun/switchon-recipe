f9b23587a112772d09760c7d2d47256a47579ce5
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-08-21 01:57:31 +0900

    💬 저장하지 않은 레시피는 한 달 이후 자동으로 삭제된다는 안내 문구 추가

--- components/home/RecentRecipes.tsx
+++ components/home/RecentRecipes.tsx
@@ -20,7 +20,10 @@ export default function RecentRecipes({ refreshing }: { refreshing: boolean }) {
 
   return (
     <View className="">
-      <Text className="mb-6 text-2xl font-bold">최근 만든 레시피</Text>
+      <Text className="mb-1 text-2xl font-bold">최근 만든 레시피</Text>
+      <Text className="mb-6 text-neutral-500">
+        저장하지 않은 레시피는 한 달이 지나면 자동으로 삭제돼요.
+      </Text>
       {refreshing ? (
         <ActivityIndicator className="h-48" size="large" color={colors.green[500]} />
       ) : (
