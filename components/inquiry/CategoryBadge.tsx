059b58b42f690b0f570042ef50f0925bcb585f01
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-09-12 00:30:43 +0900

    🎨 CategoryButton의 UI만 CategoryBadge로 분리

---
+++ components/inquiry/CategoryBadge.tsx
@@ -0,0 +1,56 @@
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+import { Text } from 'components/common/Text';
+import { View } from 'react-native';
+import colors from 'tailwindcss/colors';
+import { InquiryCategory } from 'types/database';
+
+interface CategoryBadgeProps {
+  category: InquiryCategory | null;
+  active?: boolean;
+}
+
+const baseIconColor = colors.neutral[400];
+const baseTextColor = 'text-neutral-400';
+const baseBorderColor = 'border-neutral-400';
+
+const baseViewClassName = 'self-start flex-row items-center rounded-xl border px-3 py-2';
+
+export function CategoryBadge({ category, active = true }: CategoryBadgeProps) {
+  if (category === '일반 문의') {
+    return (
+      <View className={`${baseViewClassName} ${active ? 'border-green-600' : baseBorderColor}`}>
+        <FontAwesome5
+          name="question-circle"
+          size={16}
+          color={active ? colors.green[600] : baseIconColor}
+        />
+        <Text className={`ml-1 ${active ? 'text-green-600' : baseTextColor}`}>{category}</Text>
+      </View>
+    );
+  }
+  if (category === '버그 신고') {
+    return (
+      <View className={`${baseViewClassName} ${active ? 'border-red-500' : baseBorderColor}`}>
+        <MaterialIcons
+          name="bug-report"
+          size={16}
+          color={active ? colors.red[500] : baseIconColor}
+        />
+        <Text className={`ml-1 ${active ? 'text-red-500' : baseTextColor}`}>{category}</Text>
+      </View>
+    );
+  }
+  if (category === '기능 요청') {
+    return (
+      <View className={`${baseViewClassName} ${active ? 'border-yellow-500' : baseBorderColor}`}>
+        <MaterialIcons
+          name="lightbulb"
+          size={16}
+          color={active ? colors.yellow[500] : baseIconColor}
+        />
+        <Text className={`ml-1 ${active ? 'text-yellow-500' : baseTextColor}`}>{category}</Text>
+      </View>
+    );
+  }
+  return null;
+}
--- components/inquiry/CategoryButton.tsx
+++ components/inquiry/CategoryButton.tsx
@@ -1,8 +1,6 @@
-import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import RippleButton from 'components/common/RippleButton';
-import { Text } from 'components/common/Text';
-import colors from 'tailwindcss/colors';
 import { InquiryCategory } from 'types/database';
+import { CategoryBadge } from './CategoryBadge';
 
 interface CategoryButtonProps {
   category: InquiryCategory;
@@ -17,50 +15,9 @@ export default function CategoryButton({
 }: CategoryButtonProps) {
   const isSelected = currentCategory === category;
 
-  const getCategoryBorderColor = () => {
-    if (category === '일반 문의') return 'border-green-600';
-    if (category === '버그 신고') return 'border-red-500';
-    if (category === '기능 요청') return 'border-yellow-500';
-    return '';
-  };
-
-  const getCategoryTextColor = () => {
-    if (category === '일반 문의') return 'text-green-600';
-    if (category === '버그 신고') return 'text-red-500';
-    if (category === '기능 요청') return 'text-yellow-500';
-    return '';
-  };
-
   return (
-    <RippleButton
-      outerClassName={`border ${isSelected ? getCategoryBorderColor() : 'border-neutral-400'}`}
-      onPress={onPress}
-      className="flex-row px-3 py-2">
-      {category === '일반 문의' && (
-        <FontAwesome5
-          name="question-circle"
-          size={16}
-          color={isSelected ? colors.green[600] : colors.neutral[400]}
-        />
-      )}
-      {category === '버그 신고' && (
-        <MaterialIcons
-          name="bug-report"
-          size={16}
-          color={isSelected ? colors.red[500] : colors.neutral[400]}
-        />
-      )}
-      {category === '기능 요청' && (
-        <MaterialIcons
-          name="lightbulb"
-          size={16}
-          color={isSelected ? colors.yellow[500] : colors.neutral[400]}
-        />
-      )}
-
-      <Text className={`ml-1 ${isSelected ? getCategoryTextColor() : 'text-neutral-400'}`}>
-        {category}
-      </Text>
+    <RippleButton rippleColor="transparent" onPress={onPress}>
+      <CategoryBadge category={category} active={isSelected} />
     </RippleButton>
   );
 }
