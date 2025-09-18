48685066b57a68fe88c1777cb7a56f2ba13cb887
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-09-19 07:37:22 +0900

    ✨ 시작 날짜 재설정 시, 새로운 주차가 된다면 주차 완료 팝업을 띄우는 기능 추가

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -14,6 +14,7 @@ import { useCallback, useRef, useState } from 'react';
 import { RefreshControl, ScrollView, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useUserStore } from 'stores/userStore';
+import { useWeekCompletePopupStore } from 'stores/weekCompletePopupStore';
 import { getWeekAndDay } from 'utils/date';
 import { RecipeAPI } from '../../../api/RecipeAPI';
 import { UserAPI } from '../../../api/UserAPI';
@@ -43,7 +44,9 @@ export default function HomeScreen() {
   };
 
   const updateStartDate = async () => {
+    const { week: prevWeek, open, setWeek } = useWeekCompletePopupStore.getState();
     const { week } = getWeekAndDay(selectedDate);
+    setWeek(week);
 
     const filteredLowIngredients = lowIngredients.filter((ingredient) => ingredient.week <= week);
     if (lowIngredients.length !== filteredLowIngredients.length) {
@@ -59,6 +62,10 @@ export default function HomeScreen() {
 
     if (bottomSheetRef) {
       bottomSheetRef.current?.close();
+    }
+
+    if (week < 5 && prevWeek < week) {
+      open();
     }
   };
 
