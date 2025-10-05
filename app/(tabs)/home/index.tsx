431e291547964a3d27f8f4ce4f9d09e232b3bf64
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-10-05 15:36:58 +0900

    ♻️ 시작날짜 재설정 바텀시트 컴포넌트 분리

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,73 +1,23 @@
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-import { CustomBottomSheet } from 'components/common/CustomBottomSheet';
-import CustomCalendar from 'components/common/CustomCalendar';
-import RippleButton from 'components/common/RippleButton';
-import { Text } from 'components/common/Text';
+import BottomSheet from '@gorhom/bottom-sheet';
 import HomeHeader from 'components/home/HomeHeader';
 import RecentRecipes from 'components/home/RecentRecipes';
 import RecipeCreation from 'components/home/RecipeCreation';
 import SavedRecipes from 'components/home/SavedRecipes';
+import { StartDateBottomSheet } from 'components/home/StartDateBottomSheet';
 import { latestNotices } from 'const/notices';
-import useKoreanToday from 'hooks/useKoreanToday';
-import { useSelectedIngredients } from 'hooks/useSelectedIngredients';
 import { useCallback, useRef, useState } from 'react';
-import { RefreshControl, ScrollView, TouchableOpacity, View } from 'react-native';
+import { RefreshControl, ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useUserStore } from 'stores/userStore';
-import { useWeekCompletePopupStore } from 'stores/weekCompletePopupStore';
-import { getWeekAndDay } from 'utils/date';
 import { RecipeAPI } from '../../../api/RecipeAPI';
-import { UserAPI } from '../../../api/UserAPI';
 import { LatestNotice } from '../../../components/home/LatestNotice';
 import { useRecipeStore } from '../../../stores/recipeStore';
 
 export default function HomeScreen() {
   const bottomSheetRef = useRef<BottomSheet>(null);
 
-  const { selectedIngredients: lowIngredients, upsertIngredients: upsertLowIngredients } =
-    useSelectedIngredients({ type: 'low' });
-  const { selectedIngredients: zeroIngredients, upsertIngredients: upsertZeroIngredients } =
-    useSelectedIngredients({ type: 'zero' });
-
   const [refreshing, setRefreshing] = useState(false);
   const setRecentRecipes = useRecipeStore((state) => state.setRecentRecipes);
   const setSavedRecipes = useRecipeStore((state) => state.setSavedRecipes);
-
-  const today = useKoreanToday();
-  const startDate = useUserStore((state) => state.start_date);
-  const [calendarKey, setCalendarKey] = useState(false); // 캘린더 리렌더링용 키
-  const [selectedDate, setSelectedDate] = useState<string>(startDate ?? today);
-
-  const setDateToToday = () => {
-    setSelectedDate(today);
-    setCalendarKey((prev) => !prev);
-  };
-
-  const updateStartDate = async () => {
-    const { week: prevWeek, open, setWeek } = useWeekCompletePopupStore.getState();
-    const { week } = getWeekAndDay(selectedDate);
-    setWeek(week);
-
-    const filteredLowIngredients = lowIngredients.filter((ingredient) => ingredient.week <= week);
-    if (lowIngredients.length !== filteredLowIngredients.length) {
-      upsertLowIngredients(filteredLowIngredients);
-    }
-
-    const filteredZeroIngredients = zeroIngredients.filter((ingredient) => ingredient.week <= week);
-    if (zeroIngredients.length !== filteredZeroIngredients.length) {
-      upsertZeroIngredients(filteredZeroIngredients);
-    }
-
-    await UserAPI.updateStartDate(selectedDate);
-
-    if (bottomSheetRef) {
-      bottomSheetRef.current?.close();
-    }
-
-    if (week < 5 && prevWeek < week) {
-      open();
-    }
-  };
 
   const handleRefresh = useCallback(async () => {
     setRefreshing(true);
@@ -111,30 +61,7 @@ export default function HomeScreen() {
         </View>
       </ScrollView>
 
-      <CustomBottomSheet ref={bottomSheetRef}>
-        <BottomSheetView className="px-5">
-          <View className="flex-row items-center justify-between">
-            <Text className="mb-2 text-2xl font-bold">시작 날짜 재설정</Text>
-            <TouchableOpacity
-              className="rounded-lg border border-green-600 px-3 py-2.5"
-              onPress={setDateToToday}>
-              <Text className="text-green-600">오늘로 설정</Text>
-            </TouchableOpacity>
-          </View>
-          <Text className="mb-6 text-neutral-500">날짜를 선택해주세요</Text>
-          <CustomCalendar
-            calendarKey={String(calendarKey)}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-          />
-          <RippleButton
-            outerClassName="mb-10"
-            className="w-full bg-green-600 py-4"
-            onPress={updateStartDate}>
-            <Text className="text-lg font-semibold text-white">날짜 재설정하기</Text>
-          </RippleButton>
-        </BottomSheetView>
-      </CustomBottomSheet>
+      <StartDateBottomSheet bottomSheetRef={bottomSheetRef} />
     </SafeAreaView>
   );
 }
