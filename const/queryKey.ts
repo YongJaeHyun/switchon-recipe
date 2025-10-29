c3b030b3185caf94dfd8c66dccccc144ede3f717
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-10-29 10:45:21 +0900

    💬 쿼리키 추가 (recentRecipe, recipeCards)

--- const/queryKey.ts
+++ const/queryKey.ts
@@ -1,5 +1,7 @@
 export const QueryKey = {
   inquiries: 'inquiries',
   selectedIngredients: 'selectedIngredients',
+  recentRecipes: 'recentRecipes',
   savedRecipes: 'savedRecipes',
+  recipeCards: 'recipeCards',
 };
