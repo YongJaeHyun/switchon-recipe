1e95110c32115e62ab2754da19bf1fc216e18db5
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-10-31 10:08:03 +0900

    💄 새로 추가된 탐색 탭 옆에 'NEW' 뱃지 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import RippleButton from 'components/common/RippleButton';
+import { Text } from 'components/common/Text';
 import { Tabs } from 'expo-router';
+import { View } from 'react-native';
 import colors from 'tailwindcss/colors';
 
 function TabLayout() {
@@ -34,7 +36,14 @@ function TabLayout() {
         name="explore"
         options={{
           title: '탐색',
-          tabBarIcon: ({ color }) => <MaterialIcons name="explore" size={28} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <View className="relative">
+              <MaterialIcons name="explore" size={28} color={color} />
+              <View className="absolute -right-8 top-0 h-4 min-w-4 items-center justify-center rounded-full bg-red-500 px-1">
+                <Text className="text-center text-[10px] font-bold text-white">NEW</Text>
+              </View>
+            </View>
+          ),
         }}
       />
     </Tabs>
