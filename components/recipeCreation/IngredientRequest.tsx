1429ccb102fb179d7d73bbfeeef86ee9b5db2a10
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-11-10 04:49:24 +0900

    ✨ 재료 요청 시, 이미 존재하는 재료일 경우 에러 메시지 출력

--- components/recipeCreation/IngredientRequest.tsx
+++ components/recipeCreation/IngredientRequest.tsx
@@ -2,6 +2,11 @@ import { FontAwesome6 } from '@expo/vector-icons';
 import { sendSlackMessage } from 'api/slack';
 import RippleButton from 'components/common/RippleButton';
 import { Text } from 'components/common/Text';
+import {
+  firstWeekIngredients,
+  secondWeekIngredients,
+  thirdWeekIngredients,
+} from 'const/ingredients';
 import { useLastPathname } from 'hooks/useLastPathname';
 import { useState } from 'react';
 import { Modal, TextInput, TouchableWithoutFeedback, View } from 'react-native';
@@ -19,6 +24,12 @@ export function IngredientRequest() {
   const [visible, setVisible] = useState(false);
   const [name, setName] = useState('');
   const [errors, setErrors] = useState<IngredientRequestError | null>(null);
+
+  const allIngredientNames = [
+    ...firstWeekIngredients,
+    ...secondWeekIngredients,
+    ...thirdWeekIngredients,
+  ].map((ingredient) => ingredient.name);
 
   const open = () => setVisible(true);
   const close = () => {
@@ -38,6 +49,7 @@ export function IngredientRequest() {
     if (!trimmedName) newErrors.name = '재료명을 입력해주세요.';
     else if (!isCompletedHangul(trimmedName))
       newErrors.name = '재료명은 중간에 자음이나 모음이 단독으로 올 수 없어요.';
+    else if (allIngredientNames.includes(trimmedName)) newErrors.name = '이미 존재하는 재료예요.';
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -78,7 +90,7 @@ export function IngredientRequest() {
                 검토 후, 스위치온 다이어트에 적합한 재료라면 추가돼요.
               </Text>
 
-              <View className="mb-8">
+              <View className="mb-4">
                 <TextInput
                   className="rounded-xl border border-neutral-300 px-3"
                   placeholder="재료명을 입력해주세요 (최대 10글자)"
@@ -89,9 +101,14 @@ export function IngredientRequest() {
                   multiline={false}
                   textAlignVertical="center"
                 />
-                {errors?.name && <Text className="mt-1 text-sm text-red-500">{errors.name}</Text>}
+                {errors?.name && (
+                  <Text className="ml-1 mt-1 text-sm text-red-500">{errors.name}</Text>
+                )}
               </View>
 
+              <Text className="mb-4 text-sm text-neutral-500">
+                🕒 검토에는 최대 2일 정도 걸려요.
+              </Text>
               <View className="flex-row gap-4">
                 <RippleButton
                   onPress={close}
