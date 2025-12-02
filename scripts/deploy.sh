dc8ce805960ff96d54eb19e38d2d676809118c8d
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2025-12-03 07:33:50 +0900

    🔨 배포 후 버전 업데이트가 3시간 이후에 진행되도록 수정
    배포 후, 모든 사용자에게 내용이 보여지기까지 짧게는 30분, 길게는 2시간까지 걸린다고 해서 넉넉하게 3시간으로 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 if eas build --platform android --profile production --auto-submit; then
-  node ./scripts/updateVersion.js
+  node ./scripts/updateVersion.js &
+  echo "✅ Build Success (updateVersion.js is running in background)"
 else
   echo "❌ Build failed. Skipping updateVersion.js"
 fi
